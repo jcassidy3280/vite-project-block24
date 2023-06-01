a575c0e82e729f690005ec6e80d884da02ba0817
jcassidy3280 <jcassidy3280@gmail.com>
2023-06-01 09:08:34 -0400

    block 24 assignment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from 'react'
 import '../App.css'
+import {puppyList} from '../data'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
-   <div>Hello</div>
+   <div className="App">
+    {
+      puppies.map((puppy) => {
+      return (
+      <p onClick={()=>{setFeatPupId(puppy.id)}}  key={puppy.id}>
+      {puppy.name}</p>)
+   })
+    }
+    { featPupId && <div className="Featured">
+      <h2>{featuredPup.name}</h2>
+          <p>
+            <p>Age: {featuredPup.age}</p>
+            <p>Email: {featuredPup.email}</p>
+          </p>
+      </div> } 
+   </div>
   )
 }
 
 export default App
+
+
